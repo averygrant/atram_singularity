769244b1e6199dfd0bc519d050879c2c86715d2c
Avery Grant <averygrant@wallace.biology.unr.edu>
2021-03-12 14:30:29 -0500

    preprocessor script for example aTRAM run

---
+++ run_preprocessor.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/sh
+
+python3 /aTRAM/atram_preprocessor.py --end-1 /aTRAM/doc/data/tutorial_end_1.fasta --end-2 /aTRAM/doc/data/tutorial_end_2.fasta -b /aTRAM/atram_lib/lib_test -t /aTRAM/temp
+
