eb35cedae4f88108a4afad09cc741434266284d6
Avery Grant <averygrant@wallace.biology.unr.edu>
2021-03-12 14:31:19 -0500

    atram bash script for example aTRAM run

---
+++ run_atram.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/sh
+
+python3 /aTRAM/atram.py -b /aTRAM/atram_lib/lib_test -Q /aTRAM/doc/data/tutorial-query.pep.fasta -a velvet -o /aTRAM/atram_out/test/ -t /aTRAM/temp --keep-temp-dir
+
