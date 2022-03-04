6df1aab9ce0db47a706c29b0f734177aa58f6b60
Avery Grant <46731868+averygrant@users.noreply.github.com>
2022-03-04 15:59:40 -0800

    Update run_atram.sh

--- run_atram.sh
+++ run_atram.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
 
-python3 /aTRAM/atram.py -b /aTRAM/atram_lib/lib_test -Q /aTRAM/doc/data/tutorial-query.pep.fasta -a velvet -o /aTRAM/atram_out/test/ -t /aTRAM/temp --keep-temp-dir
+singularity exec /path/to/sif_file/aTRAM.sif python3 /aTRAM/atram.py -b /path/to/atram_lib/<prefix> -t /path/to/temp/ -Q /path/to/target/target.fasta -a <assembler of choice> -o /path/to/atram_out/
 
