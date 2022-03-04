1c2f92e6c6fc10d1244e71e4a238bc04f09a6b37
Avery Grant <46731868+averygrant@users.noreply.github.com>
2022-03-04 15:57:25 -0800

    Update run_preprocessor.sh

--- run_preprocessor.sh
+++ run_preprocessor.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/sh
 
-singularity exec /path/to/sif_file/aTRAM_test.sif python3 /aTRAM/atram_preprocessor.py --end-1 /path/to/read1/data/R1pair.trim.fastq --end-2 /path/to/read2/data/R2pair.trim.fastq -b /paht/to/atram_lib/<prefix_for_files> -t /path/to/temp
+singularity exec /path/to/sif_file/aTRAM.sif python3 /aTRAM/atram_preprocessor.py --end-1 /path/to/read1/data/R1pair.trim.fastq --end-2 /path/to/read2/data/R2pair.trim.fastq -b /paht/to/atram_lib/<prefix_for_files> -t /path/to/temp
